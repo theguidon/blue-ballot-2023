e3f5eea5c6d389e2bda88da51f4cbaee65926ce3
Jose Luis Bautista <jose.luis.bautista@obf.ateneo.edu>
2023-03-09 01:06:51 +0800

    Added react router and some default pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react";
 import "./index.css";
-import "./fonts.css"
+import "./fonts.css";
+import { Link, Routes, Route, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Candidates from "./pages/Candidates";
+import Archives from "./pages/Archives";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={Layout()}>
+          <Route path="/" element={<Home />} />
+          <Route path="/candidates" element={<Candidates />} />
+          <Route path="/archives" element={<Archives />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
+//TODO: PUT NAV BAR AND FOOTER HERE
+function Layout() {
   return (
     <div>
-      <p className="text-3xl text-red-500 underline text-center">
-        Hello world!
-      </p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/candidates">Candidates</Link>
+          </li>
+          <li>
+            <Link to="/archives">Archives</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
     </div>
   );
 }
