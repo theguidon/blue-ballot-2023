e89947aa988899f7004d2b91769710e5ee9d7ee9
chester <rey.tan@obf.ateneo.edu>
2023-03-09 17:14:43 +0800

    added shadow overlay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <header className="bg-gradient-to-r from-bb_blue via-bb_orange to-bb_yellow flex py-4 top-0 z-50 w-full px-12  tablet:px-4">
+    <header className="bg-gradient-to-r from-bb_blue via-bb_orange to-bb_yellow flex py-3 top-0 z-50 w-full px-12  tablet:px-4">
       <div
         className="cursor-pointer my-auto"
         onClick={() => {
@@ -27,9 +27,10 @@ const Header = () => {
         </svg>
       </div>
       <div className="mx-auto">
-        <img className="h-8 mr-8" src={purewhite4x1} alt="Blue Ballot" />
+        <img className="h-10 mr-8" src={purewhite4x1} alt="Blue Ballot" />
       </div>
       <NavMenu menu={menu} setMenu={setMenu} />
+      <div className={`w-screen h-screen bg-black  top-0 left-0 opacity-80 ${menu ? "absolute" : "hidden"}`}></div>
     </header>
   );
 };
